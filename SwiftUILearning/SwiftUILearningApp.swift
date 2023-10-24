95ad759d2ba668108b7aa8edcdf3b14ce48de74d
Agung Nawawi <agung@fairtech.com.sg>
2023-10-24 10:33:53 +0700

    Initial Commit

---
+++ SwiftUILearning/SwiftUILearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearningApp.swift
+//  SwiftUILearning
+//
+//  Created by Agung Nawawi on 24/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
